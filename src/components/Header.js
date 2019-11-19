1bd4820f9dc66650f363440708069335fd5e0275
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-19 14:53:55 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+function Header(){
+  return (
+    <div>
+      <div>
+        <h1>Logo</h1>
+      </div>
+      <div>
+        <div>Meu perfil</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
